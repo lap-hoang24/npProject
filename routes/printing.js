928de601d864354b58da0db90cc2cab9123a01e5
Lap Hoang <52751449+lap-hoang24@users.noreply.github.com>
2021-03-23 16:15:07 +0100

    more testing

--- routes/printing.js
+++ routes/printing.js
@@ -2,7 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-   res.send(req, "cd printing test success")
+   try {
+      const api = "https://api.seatgeek.com/2/events?";
+      const apiKey = "client_id=MjEzNjIzNTl8MTYwMzM3ODg3OS42NDc4ODU2&per_page=";
+      const perPage = "100"
+
+      const response = await fetch(api + apiKey + perPage);
+      let data = await response.json();
+      data = data.events;
+
+      res.render('pages/upcoming_events', { data })
+   } catch (err) {
+      console.log(err);
+   }
 })
 
 
